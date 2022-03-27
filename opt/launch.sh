406f1a884407c12e75313e74e03a6754f9577d69
QiukuZ <zhangqk98@zju.edu.cn>
2022-03-27 11:04:10 +0800

    add init

--- opt/launch.sh
+++ opt/launch.sh
@@ -10,5 +10,7 @@ NOHUP_FILE=$CKPT_DIR/log
 echo CKPT $CKPT_DIR
 echo LOGFILE $NOHUP_FILE
 
-CUDA_VISIBLE_DEVICES=$2 nohup python -u opt.py -t $CKPT_DIR ${@:3} > $NOHUP_FILE 2>&1 &
+echo ${@:3}
+# CUDA_VISIBLE_DEVICES=$2 nohup python -u opt.py -t $CKPT_DIR ${@:3} > $NOHUP_FILE 2>&1 &
+# python -u opt.py -t $CKPT_DIR ${@:3} > $NOHUP_FILE
 echo DETACH
